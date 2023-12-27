5cccb54771def1e8f278494fc47540daa3cb19f6
eryalito <eryalito@gmail.com>
2023-12-27 16:30:45 +0100

    feat: initial e2e tests

---
+++ e2e/test.sh
@@ -0,0 +1,11 @@
+set -e
+
+echo "#############################################"
+echo "#                                           #"
+echo "#    Starting e2e tests...                  #"
+echo "#                                           #"
+echo "#############################################"
+
+SCRIPT_DIR="$(dirname "$0")"
+/bin/bash "$SCRIPT_DIR/tests/namespaced.sh"
+/bin/bash "$SCRIPT_DIR/tests/clusterscoped.sh"
