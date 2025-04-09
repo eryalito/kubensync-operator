73e206c981431bd6ccdc2396e0b33defb8da29f3
eryalito <eryalito@gmail.com>
2025-04-09 12:56:02 +0200

    fix[e2e]: dist path

--- e2e/pre.sh
+++ e2e/pre.sh
@@ -6,7 +6,7 @@ echo "#    Starting e2e tests setup...            #"
 echo "#                                           #"
 echo "#############################################"
 
-kubectl apply -f dist/installer.yaml
+kubectl apply -f dist/install.yaml
 kubectl apply -f dist/rbac.yaml
 # wait for pods to get ready
 kubectl rollout status -n kubensync-system deployment controller-manager
