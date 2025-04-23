6a32d5d350d14ef2c3974a0c2518446c6fa2f0ff
eryalito <eryalito@gmail.com>
2025-04-23 19:33:19 +0200

    fix: linter

--- internal/reconciler/utils.go
+++ internal/reconciler/utils.go
@@ -1,16 +1,12 @@
 package reconciler
 
 import (
-	"context"
 	"regexp"
 
-	"github.com/eryalito/kubensync-operator/api/v1alpha1"
 	automationv1alpha1 "github.com/eryalito/kubensync-operator/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/client-go/kubernetes"
 )
 
 func namespaceMatchesManagedResource(namespace *corev1.Namespace, managedResource *automationv1alpha1.ManagedResource) (bool, error) {
@@ -32,22 +28,8 @@ func namespaceMatchesManagedResource(namespace *corev1.Namespace, managedResourc
 	return true, nil
 }
 
-func isNamespacePresent(name string, clientset *kubernetes.Clientset) (bool, error) {
-	if name == "" {
-		return false, nil
-	}
-	_, err := clientset.CoreV1().Namespaces().Get(context.TODO(), name, metav1.GetOptions{})
-	if err != nil {
-		if errors.IsNotFound(err) {
-			return false, nil
-		}
-		return false, err
-	}
-	return true, nil
-}
-
 // removeResource removes a CreatedResource from the slice if it exists.
-func removeResource(resources []v1alpha1.CreatedResource, target v1alpha1.CreatedResource) []v1alpha1.CreatedResource {
+func removeResource(resources []automationv1alpha1.CreatedResource, target automationv1alpha1.CreatedResource) []automationv1alpha1.CreatedResource {
 	for i, resource := range resources {
 		if resource == target {
 			// Remove the element by slicing around it
